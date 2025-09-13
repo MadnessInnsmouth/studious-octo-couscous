65f42cc7a0834c34dca04e2726df4757ff8aa1b0
MadnessInnsmouth <196637697+MadnessInnsmouth@users.noreply.github.com>
2025-09-13 12:11:27 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,18 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
-        maven("https://jitpack.io")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
     }
 }
-rootProject.name = "ai-ime-merged"
+
+rootProject.name = "studious-octo-couscous"
 include(":app")
