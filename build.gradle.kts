05319c2856561180fecc1a9fdf36aae573af2573
MadnessInnsmouth <196637697+MadnessInnsmouth@users.noreply.github.com>
2025-09-13 10:50:19 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,5 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
+
+// No repositories block here (because using FAIL_ON_PROJECT_REPOS in settings.gradle.kts)
