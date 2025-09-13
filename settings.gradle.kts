9ba26a542251fd029169cc5a6a86e2f648117ae0
MadnessInnsmouth <196637697+MadnessInnsmouth@users.noreply.github.com>
2025-09-13 10:53:26 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,20 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        // Vosk speech recognition artifacts are hosted here
+        maven { url = uri("https://alphacephei.com/maven/") }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // Required for Vosk
+        maven { url = uri("https://alphacephei.com/maven/") }
     }
 }
+
 rootProject.name = "ai-ime-merged"
 include(":app")
